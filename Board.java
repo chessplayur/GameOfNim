b42bcbf169b7fa7b0465f791dd6b5f4c91a6019b
chessplayur <ruvninja@gmail.com>
2025-10-31 08:37:55 -0700

    Update Board.java

--- Board.java
+++ Board.java
@@ -2,7 +2,10 @@ import java.util.Random;
 
 public class Board {
     private static int pieces;
-    // Create method
+    
+    /**
+     * Initializes the number of pieces on the board to a random value between 10 and 50.
+     */
     public static void populate() {
         Random rand = new Random();
         pieces = rand.nextInt(41) + 10; // random number between 10 and 50
@@ -12,9 +15,13 @@ public class Board {
         return pieces;
     }
 
+    /**
+     * Removes a specified number of pieces from the pile.
+     * FIX: The check if (pieces < 0) pieces = 1; was removed, as it prevented the game
+     * from ending when the piece count naturally hit 0.
+     * @param num The number of pieces to remove.
+     */
     public static void removePieces(int num) {
         pieces -= num;
-        if (pieces < 0) pieces = 1;
     }
 }
-
